cc7894f38d8438d745538751c615f3818b4dc608
LieuND <LieuND@saisystem.vn>
2020-08-14 10:32:30 +0700

    upd SQL

--- lisinhami/SQL/lisinhami_db.sql
+++ lisinhami/SQL/lisinhami_db.sql
@@ -2,10 +2,10 @@
 -- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Aug 13, 2020 at 10:48 AM
--- Server version: 10.4.13-MariaDB
--- PHP Version: 7.2.32
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th8 14, 2020 lúc 12:31 PM
+-- Phiên bản máy phục vụ: 10.4.13-MariaDB
+-- Phiên bản PHP: 7.2.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,21 +18,22 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `lisinhami_db`
+-- Cơ sở dữ liệu: `lisinhami_db`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `t_address`
+-- Cấu trúc bảng cho bảng `t_address`
 --
 
 CREATE TABLE `t_address` (
-  `id` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Id địa chỉ',
+  `id` int(11) NOT NULL COMMENT 'Id địa chỉ',
+  `id_user` varchar(30) COLLATE utf8_unicode_ci NOT NULL COMMENT 'id user',
   `address_cd1` tinyint(1) NOT NULL DEFAULT 1 COMMENT 'Mã địa chỉ chính',
   `address_cd2` tinyint(1) NOT NULL DEFAULT 2 COMMENT 'Mã địa chỉ phụ',
-  `address1` varchar(500) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Địa chỉ chính',
-  `address2` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Địa chỉ phụ',
+  `address1` varchar(150) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Địa chỉ chính',
+  `address2` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Địa chỉ phụ',
   `del_flg` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Cờ xóa(0: Không xóa, 1: Xóa)',
   `create_datetime` datetime NOT NULL DEFAULT current_timestamp() COMMENT 'Thời gian tạo',
   `update_datetime` datetime NOT NULL DEFAULT current_timestamp() COMMENT 'Thời gian cập nhật'
@@ -41,13 +42,13 @@ CREATE TABLE `t_address` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `t_image`
+-- Cấu trúc bảng cho bảng `t_image`
 --
 
 CREATE TABLE `t_image` (
-  `id` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT 'id',
-  `id_prd` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT 'id Sản phẩm',
-  `img_url` varchar(300) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Link hình ảnh',
+  `id` int(11) NOT NULL COMMENT 'id',
+  `id_prd` int(11) NOT NULL COMMENT 'id Sản phẩm',
+  `img_url` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Link hình ảnh',
   `top_flg` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Cờ set hình ảnh mặc định (0: không set, 1: set default)',
   `banner_flg` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Cờ set hình ảnh banner (0: không set, 1: set)',
   `del_flg` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Cờ xóa (0: không xóa, 1: xóa)',
@@ -58,17 +59,17 @@ CREATE TABLE `t_image` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `t_odrd`
+-- Cấu trúc bảng cho bảng `t_order_detail`
 --
 
-CREATE TABLE `t_odrd` (
-  `id` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT 'id',
-  `id_odrh` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT 'mã đơn hàng',
-  `id_prd` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT 'mã sản phẩm',
+CREATE TABLE `t_order_detail` (
+  `id` int(11) NOT NULL COMMENT 'id',
+  `id_odrh` int(11) NOT NULL COMMENT 'mã đơn hàng',
+  `id_product` int(11) NOT NULL COMMENT 'mã sản phẩm',
   `amount` int(11) NOT NULL DEFAULT 1 COMMENT 'Số lượng',
   `price` decimal(10,0) NOT NULL DEFAULT 0 COMMENT 'Đơn giá',
   `tax` decimal(10,0) NOT NULL DEFAULT 0 COMMENT 'Thuế suất',
-  `del_flg` tinyint(4) NOT NULL DEFAULT 0 COMMENT 'cờ xóa (0: không xóa, 1: xóa)',
+  `del_flg` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'cờ xóa (0: không xóa, 1: xóa)',
   `create_datetime` datetime NOT NULL DEFAULT current_timestamp() COMMENT 'Thời gian tạo',
   `update_datetime` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'Thời gian update'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -76,22 +77,23 @@ CREATE TABLE `t_odrd` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `t_odrh`
+-- Cấu trúc bảng cho bảng `t_order_header`
 --
 
-CREATE TABLE `t_odrh` (
-  `id` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT 'id',
-  `odr_cd` varchar(300) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Mã đơn hàng',
-  `id_user` varchar(200) COLLATE utf8_unicode_ci NOT NULL COMMENT 'mã user',
+CREATE TABLE `t_order_header` (
+  `id` int(11) NOT NULL COMMENT 'id',
+  `id_user` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'mã user',
   `odr_date` date NOT NULL DEFAULT current_timestamp() COMMENT 'Ngày tạo',
-  `paymnt_method` varchar(300) COLLATE utf8_unicode_ci NOT NULL COMMENT 'phương thức thanh toán',
-  `shipping_unit` varchar(300) COLLATE utf8_unicode_ci NOT NULL COMMENT 'đơn vị vận chuyển',
+  `paymnt_method` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT 'phương thức thanh toán',
+  `shipping_unit` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT 'đơn vị vận chuyển',
   `fee` decimal(10,0) NOT NULL DEFAULT 0 COMMENT 'phí ship',
-  `address` varchar(500) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Địa chỉ',
-  `reciever` varchar(300) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Tên người nhận hàng',
-  `phone` varchar(15) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Số điện thoại người nhận hàng',
-  `note` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Ghi chú đơn hàng',
+  `address` varchar(150) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Địa chỉ',
+  `reciever` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Tên người nhận hàng',
+  `phone` varchar(10) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Số điện thoại người nhận hàng',
+  `note` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Ghi chú đơn hàng',
   `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT 'Trạng thái đơn hàng (\r\n1: chờ xử lý xác nhận đơn hàng\r\n2: đã xử lý, chờ xuất hàng.\r\n3: đã xuất hàng chờ vận chuyển\r\n4: đang vận chuyển, chờ giao hàng\r\n5: Đang giao hàng\r\n6: Nhận hàng, hoàn thành đơn hàng\r\n0: Hủy đơn hàng\r\n)',
+  `sample_flg` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Cờ xác định đơn hàng sản phẩm sample (0: không phải là đơn hàng sample, 1: là đơn hàng sản phẩm sample)',
+  `login_flg` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Cờ xác định mua hàng đăng nhập(0: Không đăng nhập, 1:có đăng nhập)',
   `create_datetime` datetime NOT NULL DEFAULT current_timestamp() COMMENT 'Thời gian tạo',
   `update_datetime` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'Thời gian update'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -99,13 +101,14 @@ CREATE TABLE `t_odrh` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `t_payment _method`
+-- Cấu trúc bảng cho bảng `t_payment _method`
 --
 
 CREATE TABLE `t_payment _method` (
-  `id` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT 'id',
-  `payment_cd` int(11) NOT NULL COMMENT 'mã phương thức thanh toán',
-  `method` varchar(300) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Phương thức thanh toán',
+  `id` int(11) NOT NULL COMMENT 'id',
+  `payment_cd` tinyint(3) NOT NULL COMMENT 'mã phương thức thanh toán',
+  `method_paymnt` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Phương thức thanh toán',
+  `slug` varchar(60) COLLATE utf8_unicode_ci NOT NULL COMMENT 'link thân thiện',
   `del_flg` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'cờ xóa (0: không xóa, 1: xóa)',
   `create_datetime` datetime NOT NULL DEFAULT current_timestamp() COMMENT 'thời gian tạo',
   `update_datetime` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'thời gian update'
@@ -114,12 +117,12 @@ CREATE TABLE `t_payment _method` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `t_point`
+-- Cấu trúc bảng cho bảng `t_point`
 --
 
 CREATE TABLE `t_point` (
-  `id` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT 'id',
-  `id_user` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `id` int(11) NOT NULL COMMENT 'id',
+  `id_user` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `io_flg` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'cờ xác định in_out\r\n(0: in, 1: out)',
   `point` int(11) NOT NULL DEFAULT 0 COMMENT 'Số lượng point',
   `del_flg` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Cờ xóa(0: không xóa, 1: xóa)',
@@ -130,21 +133,21 @@ CREATE TABLE `t_point` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `t_product`
+-- Cấu trúc bảng cho bảng `t_product`
 --
 
 CREATE TABLE `t_product` (
-  `id` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Id sản phẩm',
-  `prd_cd` varchar(300) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Mã sản phẩm',
-  `name` varchar(500) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Tên sản phẩm',
+  `id` int(11) NOT NULL COMMENT 'Id sản phẩm',
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Tên sản phẩm',
   `category_cd` tinyint(1) NOT NULL DEFAULT 1 COMMENT 'Mã thể loại(1:sản phẩm mỹ phẩm, 2: sản phẩm dùng thử, 3: sản phẩm quà tặng)',
   `price` decimal(10,0) NOT NULL DEFAULT 0 COMMENT 'Giá sản phẩm',
   `discount` decimal(10,0) NOT NULL DEFAULT 0 COMMENT 'Giảm giá',
   `tax` tinyint(2) NOT NULL COMMENT 'Thuế ',
   `made_in` varchar(30) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Xuất xứ',
-  `info_gen` varchar(500) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Mô tả',
+  `info_gen` varchar(200) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Mô tả',
   `info_dtl` text COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Nội dung',
   `point` int(11) NOT NULL DEFAULT 0 COMMENT 'Điểm thưởng',
+  `slug` varchar(60) COLLATE utf8_unicode_ci NOT NULL COMMENT 'link thân thiện',
   `del_flg` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Cờ xóa (0: Không xóa,  1: xóa)',
   `create_datetime` datetime NOT NULL DEFAULT current_timestamp() COMMENT 'thời gian tạo',
   `update_datetime` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'Thời gian cập nhật'
@@ -153,14 +156,14 @@ CREATE TABLE `t_product` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `t_reset_pass`
+-- Cấu trúc bảng cho bảng `t_reset_pass`
 --
 
 CREATE TABLE `t_reset_pass` (
-  `id` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Id đổi mật khẩu',
-  `id_user` varchar(200) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Id người dùng',
-  `token` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Chuỗi token',
-  `timeout` datetime NOT NULL COMMENT 'Thời gian hết hạn',
+  `id` int(11) NOT NULL COMMENT 'Id yêu cầu lấy lại mật khẩu',
+  `id_user` varchar(30) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Id người dùng',
+  `token` varchar(30) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Chuỗi token',
+  `timeout` int(11) NOT NULL COMMENT 'Thời gian hết hạn',
   `create_datetime` datetime NOT NULL DEFAULT current_timestamp() COMMENT 'Thời gian tạo',
   `update_datetime` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'Thời gian cập nhật'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -168,14 +171,15 @@ CREATE TABLE `t_reset_pass` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `t_shipping_unit`
+-- Cấu trúc bảng cho bảng `t_shipping_unit`
 --
 
 CREATE TABLE `t_shipping_unit` (
-  `id` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT 'id',
-  `ship_cd` int(11) NOT NULL COMMENT 'mã đơn vị vận chuyển',
-  `shipping_unit` varchar(300) COLLATE utf8_unicode_ci NOT NULL COMMENT 'đơn vị vận chuyển',
+  `id` int(11) NOT NULL COMMENT 'id',
+  `ship_cd` tinyint(3) NOT NULL COMMENT 'mã đơn vị vận chuyển',
+  `shipping_unit` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT 'đơn vị vận chuyển',
   `fee` decimal(10,0) NOT NULL DEFAULT 0 COMMENT 'phí vận chuyển',
+  `slug` varchar(60) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Link thân thiện',
   `del_flg` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Cờ xóa(0: không xóa, 1: xóa)',
   `create_datetime` datetime NOT NULL DEFAULT current_timestamp() COMMENT 'thời gian tạo',
   `update_datetime` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'thời gian update'
@@ -184,15 +188,14 @@ CREATE TABLE `t_shipping_unit` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `t_user`
+-- Cấu trúc bảng cho bảng `t_user`
 --
 
 CREATE TABLE `t_user` (
-  `uid` varchar(200) COLLATE utf8_unicode_ci NOT NULL COMMENT ' Email người dùng',
-  `pass` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Mật khẩu đăng nhập',
-  `full_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Họ tên người dùng',
+  `uid` varchar(30) COLLATE utf8_unicode_ci NOT NULL COMMENT ' Email người dùng',
+  `pass` varchar(30) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Mật khẩu đăng nhập',
+  `full_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Họ tên người dùng',
   `gender` tinyint(1) NOT NULL COMMENT 'Giới tính(0: Nữ, 1:Nam)',
-  `id_address` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT 'id địa chỉ',
   `phone` varchar(15) COLLATE utf8_unicode_ci NOT NULL COMMENT 'số điện thoại',
   `admin_flg` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Cờ admin(0: user, 1:admin)',
   `del_flg` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Cờ xóa(0: Không xóa, 1:Xóa)',
@@ -201,68 +204,126 @@ CREATE TABLE `t_user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Indexes for dumped tables
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `t_address`
+-- Chỉ mục cho bảng `t_address`
 --
 ALTER TABLE `t_address`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `t_image`
+-- Chỉ mục cho bảng `t_image`
 --
 ALTER TABLE `t_image`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `t_odrd`
+-- Chỉ mục cho bảng `t_order_detail`
 --
-ALTER TABLE `t_odrd`
+ALTER TABLE `t_order_detail`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `t_odrh`
+-- Chỉ mục cho bảng `t_order_header`
 --
-ALTER TABLE `t_odrh`
+ALTER TABLE `t_order_header`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `t_payment _method`
+-- Chỉ mục cho bảng `t_payment _method`
 --
 ALTER TABLE `t_payment _method`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `t_point`
+-- Chỉ mục cho bảng `t_point`
 --
 ALTER TABLE `t_point`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `t_product`
+-- Chỉ mục cho bảng `t_product`
 --
 ALTER TABLE `t_product`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `t_reset_pass`
+-- Chỉ mục cho bảng `t_reset_pass`
 --
 ALTER TABLE `t_reset_pass`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `t_shipping_unit`
+-- Chỉ mục cho bảng `t_shipping_unit`
 --
 ALTER TABLE `t_shipping_unit`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `t_user`
+-- Chỉ mục cho bảng `t_user`
 --
 ALTER TABLE `t_user`
   ADD PRIMARY KEY (`uid`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `t_address`
+--
+ALTER TABLE `t_address`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Id địa chỉ';
+
+--
+-- AUTO_INCREMENT cho bảng `t_image`
+--
+ALTER TABLE `t_image`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id';
+
+--
+-- AUTO_INCREMENT cho bảng `t_order_detail`
+--
+ALTER TABLE `t_order_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id';
+
+--
+-- AUTO_INCREMENT cho bảng `t_order_header`
+--
+ALTER TABLE `t_order_header`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id';
+
+--
+-- AUTO_INCREMENT cho bảng `t_payment _method`
+--
+ALTER TABLE `t_payment _method`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id';
+
+--
+-- AUTO_INCREMENT cho bảng `t_point`
+--
+ALTER TABLE `t_point`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id';
+
+--
+-- AUTO_INCREMENT cho bảng `t_product`
+--
+ALTER TABLE `t_product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Id sản phẩm';
+
+--
+-- AUTO_INCREMENT cho bảng `t_reset_pass`
+--
+ALTER TABLE `t_reset_pass`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Id yêu cầu lấy lại mật khẩu';
+
+--
+-- AUTO_INCREMENT cho bảng `t_shipping_unit`
+--
+ALTER TABLE `t_shipping_unit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id';
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
