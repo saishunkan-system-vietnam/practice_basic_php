503b5c761355dbfb081b3661a78ee3e4c43542c9
NhanVP <shadowin1811@gmail.com>
2020-08-17 10:09:25 +0700

    commit code 17/08/2020 AM

--- lisinhami/lisinhami/templates/layout/main.php
+++ lisinhami/lisinhami/templates/layout/main.php
@@ -4,38 +4,37 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <?= $this->Html->charset() ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>  
+    <title>
         <?= $this->fetch('title') ?>
     </title>
     <?= $this->Html->meta('icon') ?>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" >
 
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
-
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-
-    <link rel="stylesheet" href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'> 
+    <link rel="stylesheet" href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
 
     <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
 
-    <?= $this->Html->css(['normalize.min', 'milligram.min', 'cake']) ?>
-    <?= $this->Html->css(['normalize.min', 'milligram.min', 'nav']) ?>
+    <?= $this->Html->css(['nav', 'footer']) ?>
+    <?= $this->Html->css(['bootstrap.min', 'owl.carousel.min', 'smoothproducts', 'detailproduct']) ?>
+    <?= $this->Html->script(['jquery-3.3.1.min', 'jquery.cycle', 'bootstrap.min', 'owl.carousel.min', 'smoothproducts.min']) ?>
 
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
 </head>
+
 <body>
-   <?= $this->element('nav')?>
+    <?= $this->element('nav') ?>
     <main class="main">
         <div class="container">
             <?= $this->Flash->render() ?>
             <?= $this->fetch('content') ?>
         </div>
     </main>
-    <?= $this->element('footer');?>
+    <?= $this->element('footer'); ?>
 </body>
+
 </html>
