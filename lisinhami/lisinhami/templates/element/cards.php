7b97b54911c8517ec5cd2bda1078f848b27115f5
ngotasinh <sinhnt@saisystem.vn>
2020-08-18 17:43:14 +0700

    UPD Source 18/8/2020

--- lisinhami/lisinhami/templates/element/cards.php
+++ lisinhami/lisinhami/templates/element/cards.php
@@ -1,19 +1,16 @@
-
 <?php foreach ($data as $key => $item) { ?>
     <div class="col-2-ct">
         <div class="pd-box pd-box-category">
             <div class="box-images">
-                <?= $this->Html->image(isset($item->t_image['img_url'])?$item->t_image['img_url']:"/img/noproduct.png", array(
-                    "class" => "img-reponsive lazy ",
-                    'title' => $item->name,
-                    'url' => array('controller' => 'Pages', 'action' => 'detailProduct', $item->slug)
-                )); ?>
+                <a href=<?= URL_CHITIET_SANPHAM . $item->slug ?> title="<?=  $item->slug ?>">
+                    <?= $this->Html->image(isset($item->t_image['img_url']) ? $item->t_image['img_url'] : "/img/noproduct.png",['class'=> 'img-reponsive lazy']); ?>
+                </a>
             </div>
             <div class="box-content">
                 <h3>
                     <?= $this->Html->link(
                         $item->name,
-                        '/pages/detailProduct/' . $item->slug
+                        URL_CHITIET_SANPHAM.$item->slug
                     ); ?>
                 </h3>
                 <div>
@@ -21,8 +18,8 @@
                         <span class="price "><?= number_format($item->price / 10, 0, '.', ',') ?>đ</span>
                         <span class="price-drop"></span>
                     <?php } else { ?>
-                        <span class="price "><?= number_format($item->price - $item->discount / 10, 0, '.', ',') ?>đ</span>
-                        <span class="price-drop"><?= number_format($item->discount / 10, 0, '.', ',') ?>đ</span>
+                        <span class="price "><?= number_format(($item->price - $item->discount) / 10, 0, '.', ',') ?>đ</span>
+                        <span class="price-drop"><?= number_format($item->price / 10, 0, '.', ',') ?>đ</span>
                     <?php } ?>
                 </div>
             </div>
